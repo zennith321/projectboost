091f2c1b745cb5683d7ea8bac21e05b904f555a3
zennith321 <mnumrich1@gmail.com>
2020-01-29 01:41:40 -0700

    Added controls and sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+                audioSource.loop = true;
+            }
+
+        }
+        else 
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
