a749ed115d7c4342b49646b459a481e76501e0e0
zennith321 <mnumrich1@gmail.com>
2020-01-30 20:52:29 -0700

    Changed movement to be more consistent and added tags to game objects

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 10f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -23,17 +26,31 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+        {
+            switch (collision.gameObject.tag)
+            {
+              case "Friendly":
+                print("OK"); //TODO remove
+                //do nothing
+                break;
+              default:
+                print("Dead");
+                //TODO Kill the player
+                break;
+            }
+        }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.W)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
                 audioSource.loop = true;
             }
-
         }
         else
         {
@@ -45,13 +62,15 @@ public class Rocket : MonoBehaviour
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; //resume physics control of rotation
